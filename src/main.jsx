1927f811f35c3f2a81cb72510f8d95824ba98b90
GautamGulati28 <gautamgulati2809@gmail.com>
2025-07-12 10:41:55 +0530

    updated Favicon

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import ContextProvider from "./Components/NodeContext.jsx";
+import ContextProvider from "./components/NodeContext.jsx";
 import { ToastContainer } from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
